0a6c4f41dd428893ff014bd33fd1b7962fe8658e
santhosh <santhoshkumar7496@gmail.com>
2022-02-23 12:09:39 +0530

    TableView,Buttons and Label added.

---
+++ Today/Model/Remainder .swift
@@ -0,0 +1,26 @@
+//
+//  Remainder .swift
+//  Today
+//
+//  Created by Santhosh on 23/02/22.
+//
+
+import Foundation
+
+struct Remainder{
+    var title: String
+    var date: Date
+    var notes:String? = nil
+    var isComplete: Bool = false
+}
+
+extension Remainder{
+    static var testData = [Remainder(title: "Read UIKit", date: Date().addingTimeInterval(300), notes: nil, isComplete: false),
+                    Remainder(title: "Prepare for Meeting", date: Date().addingTimeInterval(400), notes: nil, isComplete: false),
+                    Remainder(title: "Migrate the Branches", date: Date().addingTimeInterval(600), notes: nil, isComplete: false),
+                    Remainder(title: "Finish Previous Tasks", date: Date().addingTimeInterval(800), notes: nil, isComplete: false),
+                    Remainder(title: "Read SwiftUI", date: Date().addingTimeInterval(450), notes: nil, isComplete: false),
+                    Remainder(title: "30min Workout", date: Date().addingTimeInterval(350), notes: nil, isComplete: false),
+                    Remainder(title: "Read Books", date: Date().addingTimeInterval(100), notes: nil, isComplete: false),
+                    Remainder(title: "Go for a walk", date: Date().addingTimeInterval(250), notes: nil, isComplete: false)]
+}
